79c5d88233d69245693fa00c6f6915b0673c8eb5
TophatTeam <supermarioandrew400@gmail.com>
2025-07-23 15:30:28 -0400

    Modified and improved the AudioCore code

--- AudioCore.c
+++ AudioCore.c
@@ -20,10 +20,29 @@ int AudioCore_Init(const char* device, unsigned int samplerate)
     int err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0);
     if (err < 0)
     {   
-        // TODO: come up with the DAW initializing
+        fprintf(stderr, "Failed to open audio device: %s\n", snd_strerror(err));
+        return -1;
     }
-#elif defined(_WIN32)
 
+    snd_pcm_hw_params_t* params;
+    snd_pcm_hw_params_alloca(&params);
+    snd_pcm_hw_params_any(handle, params);
+    snd_pcm_hw_params_set_access(handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+    snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
+    snd_pcm_hw_params_set_channels(handle, params, 2); // stereo setup
+    snd_pcm_hw_params_set_rate_near(handle, params, &sampelrate, 0);
+    
+    err = snd_pcm_hw_params(handle, params);
+    if (err < 0)
+    {
+        fprintf(stderr, "Failed to set HW params: %s\n", snd_strerror(err));
+        snd_pcm_close(handle);
+        return -2;
+    }
+
+    // TODO: Store 'handle' somewhere for later use
+#elif defined(_WIN32)
+    // TODO: Create or make a AudioCore_Init functionality for cross-platform support for windows
 #endif
     return 0;
 }
